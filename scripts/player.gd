a29eb6d8662fb2332f111b9d5071b0924c942ab5
Anej <majnik2001@gmail.com>
2026-06-25 14:47:31 +0200

    Fixed interrupts of jump and attack animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,43 @@
 extends CharacterBody2D
 
-
+# Constants
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 
+# Variables
 var is_attacking = false;
+var is_jumping = false;
 
+# References
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func start_attack_combo():
+	interrupt_jump();
+	is_attacking = true
+	animated_sprite.play("attack_combo")
+	
+func interrupt_attack():
+	if is_attacking:
+		is_attacking = false;
+	
+func start_jump():
+	interrupt_attack()
+	is_jumping = true;
+	animated_sprite.play("jump")
+	velocity.y = JUMP_VELOCITY
+
+func interrupt_jump():
+	if is_jumping:
+		is_jumping = false
 
 func read_inputs():
 	# Look for jump animation
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor() and !is_jumping:
+		start_jump()
 		
 	# Get attack animation
 	if Input.is_action_just_pressed("attack_combo") and !is_attacking:
-		is_attacking = true
-		animated_sprite.play("attack_combo")
+		start_attack_combo()
 		
 	# Get input direction
 	var direction := Input.get_axis("move_left", "move_right")
@@ -27,13 +48,17 @@ func read_inputs():
 	elif direction < 0 and !is_attacking:
 		animated_sprite.flip_h = true
 		
+	print("isAttacking: ", is_attacking)
+	print("isJumping: ", is_jumping)
+	
 	# Play animations
-	if is_on_floor():
+	if is_on_floor() and velocity.y >= 0:
+		is_jumping = false
 		if direction == 0 and !is_attacking:
 			animated_sprite.play("idle")
 		elif direction != 0 and !is_attacking:
 			animated_sprite.play("run")
-	else:
+	elif is_jumping:
 		animated_sprite.play("jump")
 		
 	# Move
