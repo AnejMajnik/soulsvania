c6b80b72b04cd83e6c53e7495388c0927db96535
Anej <majnik2001@gmail.com>
2026-08-09 17:01:25 +0200

    UI cancer changes

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,6 +2,12 @@ extends Node2D
 
 func _ready() -> void:
 	$UI.controls_requested.connect(_on_controls_requested)
+	print("Viewport visible rect: ", get_viewport().get_visible_rect())
+	print("Window size: ", DisplayServer.window_get_size())
+	print("MarginContainer global position: ", $UI/Control/MarginContainer.global_position)
+	print("MarginContainer size: ", $UI/Control/MarginContainer.size)
+	print("Control size: ", $UI/Control.size)
+	print("Control anchors: L", $UI/Control.anchor_left, " R", $UI/Control.anchor_right, " T", $UI/Control.anchor_top, " B", $UI/Control.anchor_bottom)
 	
 func _on_controls_requested():
 	$ControlsPopup.open()
